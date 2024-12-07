73618590fd47c0b150740f7364f7439b2852661b
sifatbro786 <solaimanislamsifat@gmail.com>
2024-12-07 19:28:02 +0600

    update photocard

--- components/PhotoCard.jsx
+++ components/PhotoCard.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function PhotoCard({ photo }) {
     return (
-        <Link href={`/photos/${photo.id}`} className="group">
+        <Link href={`photos/${photo.id}`} className="group">
             <Image src={photo?.url} alt={photo?.title} width={700} height={700} />
 
             <div className="title-container">
