c852c165fd43e3050fad45f69254d73b5473a2ac
sifatbro786 <solaimanislamsifat@gmail.com>
2024-12-07 14:01:26 +0600

    Navbar and logo complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: "2rem",
+                },
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
